83f2077ad8ffd43b4bca969eabb17a965c5b2d74
unknown <642mikaeldiogo@gmail.com>
2024-01-26 22:25:38 -0300

    fazendo alterações no css do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FilterMapComponent } from './components/filter-map/filter-map.component';
 import { HomeComponent } from './components/home/home.component';
 
 
